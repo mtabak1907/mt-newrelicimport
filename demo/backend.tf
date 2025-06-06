9a4a36b038713841700dc0535d891cda20c3142d
Meric Tabakoglu <113539963+mtabak1907@users.noreply.github.com>
2025-06-07 00:10:42 +0100

    backend updated

--- demo/backend.tf
+++ demo/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name =  "mt-rg01"
     storage_account_name = "mtsauksouth1"
     container_name       = "mttest"
     key                  = "mt-newrelic.demo-terraform.tfstate"
